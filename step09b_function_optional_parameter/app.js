f8b6f35af77034531db80b631e2729350aa9b4b8
Mustafa Tawab <115402517+mustafatawab@users.noreply.github.com>
2023-01-06 14:42:20 +0500

    Add files via upload

---
+++ step09b_function_optional_parameter/app.js
@@ -0,0 +1,19 @@
+"use strict";
+//Named function with optional parameters
+function buildName(firstName, lastName) {
+    if (lastName)
+        return firstName + " " + lastName;
+    else
+        return firstName;
+}
+let result1 = buildName("Bob"); //ok
+// let result2 = buildName("Bob", "Adams", "Sr.");  //error, too many parameters
+var result3 = buildName("Bob", "Adams"); //also ok
+//anonymous function type with optional parameters
+var buildName1 = function (firstName, lastName) {
+    if (lastName)
+        return firstName + " " + lastName;
+    else
+        return firstName;
+};
+console.log(buildName1);
